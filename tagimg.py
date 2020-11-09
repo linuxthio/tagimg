b105d7e6efbe28c5409ca96c76c1d3c8f55f8d3b
djibril thiongane <djibthiongane@gmail.com>
2020-11-09 21:07:59 +0000

    adding destination folder

--- tagimg.py
+++ tagimg.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
 from PIL import Image, ImageDraw, ImageFont
+import os
+from pathlib import Path
 
 
 def getname_without_ext(filename):
@@ -18,35 +21,57 @@ def tagimg(filename, prenom, nom, ratio):
     :param ratio: the ratio (float) ex: 2 or 2.0 to divide the size per 2
     :return: void
     """
+    prenom=prenom.title()
+    nom=nom.title()
     base = Image.open(filename).convert("RGBA")
     txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
-    ecart = 40 * int(ratio)
+    
+    if float(ratio)<1:
+        ecart=60
+    else:
+        ecart = 20 * int(ratio)
     fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", ecart)
     d = ImageDraw.Draw(txt)
 
     xs = base.size[0] / 2
     ys = base.size[1] / 2
+    
+    a=40 #alpha
+    d.text((xs, ys - 5*ecart), prenom, font=fnt, fill=(255, 255, 255, 255))
+    d.text((xs, ys-4*ecart), nom, font=fnt, fill=(255, 255, 255, 255))
 
-    d.text((xs, ys - ecart), prenom, font=fnt, fill=(0, 255, 0, 128))
-    d.text((xs, ys), nom, font=fnt, fill=(0, 255, 0, 123))
+    d.text((xs, ys - 3*ecart), prenom, font=fnt, fill=(0, 0, 0, 255))
+    d.text((xs, ys-2*ecart), nom, font=fnt, fill=(0, 0, 0, 255))
 
-    d.text((xs, ys + ecart), prenom, font=fnt, fill=(255, 255, 0, 128))
-    d.text((xs, ys + 2 * ecart), nom, font=fnt, fill=(255, 255, 0, 123))
+    d.text((xs, ys - ecart), prenom, font=fnt, fill=(0, 255, 0, a))
+    d.text((xs, ys), nom, font=fnt, fill=(0, 255, 0, a))
 
-    d.text((xs, ys + 3 * ecart), prenom, font=fnt, fill=(255, 0, 0, 128))
-    d.text((xs, ys + 4 * ecart), nom, font=fnt, fill=(255, 0, 0, 123))
+    d.text((xs, ys + ecart), prenom, font=fnt, fill=(255, 255, 0, a))
+    d.text((xs, ys + 2 * ecart), nom, font=fnt, fill=(255, 255, 0, a))
+
+    d.text((xs, ys + 3 * ecart), prenom, font=fnt, fill=(255, 0, 0, a))
+    d.text((xs, ys + 4 * ecart), nom, font=fnt, fill=(255, 0, 0, a))
 
     out = Image.alpha_composite(base, txt)
 
     x = base.size[0]
     y = base.size[1]
 
-
-    size = x / ratio, y / ratio
+    fratio=float(ratio)
+    size = x / fratio, y / fratio
     out.thumbnail(size, Image.ANTIALIAS)
     namefile=getname_without_ext(filename)[0]
     outer_file = namefile + "_out_."+"png"
-    out.save(outer_file)
+    DOSSIER_DEST='tagimg_out'
+    home = str(Path.home())
+    path_dest="{}/{}".format(home,DOSSIER_DEST)
+    try:
+        os.mkdir(path_dest)
+    except:
+        pass
+    
+    out.save(path_dest+'/'+outer_file)
+    print(path_dest)
     print(outer_file, base.size[0])
 
 
@@ -58,6 +83,7 @@ print(w)
 def main():
     import sys
     nb=sys.argv
+    print(len(nb))
     tagimg(nb[1],nb[2],nb[3],nb[4])
     
 if __name__=='__main__':
